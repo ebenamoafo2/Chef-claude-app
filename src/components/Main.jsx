b7ddb83edf22276f8ff3d1b48ce6f6ed0c73a72b
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-08 08:55:22 +0000

    feat: add basic ingredient form

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Main() {
+    return (
+        <section>
+            <form className="add-ingredient-form">
+                <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient" />
+                <button>Add Ingredients</button>
+            </form>
+        </section>
+    )
+}
+ 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Icon from '../assets/Chef Claude Icon.png'
+
+
+export default function Navbar (){
+    return (
+        <nav className="navbar-header">
+            <img src={ Icon } alt="Chef Claude Icon" />
+          <h1>Chef Claude</h1>
+        </nav>
+    )
+}
